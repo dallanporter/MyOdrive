afacbdcf9652b55a6d653c190e602e1e521a299b
Dallan Porter <dallan@porterdigital.com>
2023-01-04 16:30:05 -0700

    socket io and client site

--- static/js/myodrive.js
+++ static/js/myodrive.js
@@ -2,10 +2,29 @@
     var myodrive = {};
     
     myodrive.name = "foobar";
-    myodrive.init = () => {
+    myodrive.socket = null;
+    myodrive.init = (socket) => {
         console.log("Inside myodrive.init()");
+        myodrive.socket = socket;
+        myodrive.socket.on("list_odrives", myodrive.list_odrives);
+        myodrive.socket.on("odrive", myodrive.odrive);
         console.log(this);
+    };
+
+    myodrive.list_odrives = (socket_message) => {
+        console.log("Inside the mydrive.list_odrives handler.");
+
+    };
+
+    myodrive.odrive = (socket_message) => {
+        console.log("Inside the mydrive.odrive handler.");
     };
 
     window.myodrive = myodrive;
 }(jQuery));
+
+var odrive = () => {
+    return {
+        
+    };
+};
